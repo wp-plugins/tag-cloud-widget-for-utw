e201bfce6b021e386c67ea1c934c6241467d08b8
broom9 <broom9@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-05-09 07:30:56 +0000

    First version
    git-svn-id: https://plugins.svn.wordpress.org/tag-cloud-widget-for-utw/trunk@12440 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== Plugin Name ===
+Contributors: broom9
+Donate link: http://blog.broom9.com
+Tags: tag, tag_cloud, sidebar, widget
+Requires at least: 2.0.2
+Tested up to: 2.1.3
+Stable tag: trunk
+
+Adds a tag cloud sidebar widget, used with the plugin Sidebar Widget and Ultimate Tag Warrior 3
+
+== Description ==
+
+This is a sidebar widget for <a href="http://wordpress.org" title="wordpress">Wordpress</a>, which adds a tag cloud in sidebar.<br>
+It must be used together with the so popular plugin <a href="http://wordpress.org/extend/plugins/widgets/" title="wordpress plugin">sidebar widget</a> and the wonderful tag plugin <a href="http://www.neato.co.nz/archives/2007/02/04/ultimate-tag-warrior-314159265" title="ultimate tag warrior">Ultimate Tag Warrior</a>.
+
+I am using it on <a href="http://blog.broom9.com" title="my blog">my blog</a>, which is built on Wordpress 2.1.3 and UTW 3.14159265.
+
+Notes:
+* The style of tag cloud is designed by UTW, the reference page is <a title="UTW tag cloud predefiend formats" href="http://www.neato.co.nz/wp-content/plugins/UltimateTagWarrior/ultimate-tag-warrior-help-themes.html#predefinedformats">here</a>
+* Thanks for the widget ¡°<a href="http://automattic.com" title="google search widget">google search widget</a>¡°. I don¡¯t know php at all¡­just made this plugin based on it.
+
+
+== Installation ==
+
+1. Download here ¡ª <a href="http://blog.broom9.com/wp-content/uploads/2007/05/tagcloud.zip" title="Tag Cloud Widget for UTW">Tag Cloud Widget for UTW</a>
+1. Upload the ¡°tagcloud.php¡± to the path ¡°/wp-content/plugins/widgets/¡±
+1. Active this widget in Plugins control panel
+1. In the presentation-&gt;sidebar control panel, drag the tag cloud widget to your sidebar
+
+== Frequently Asked Questions ==
+Not any questions yet
+
+== Screenshots ==
+1. screenshot-1.png
---
+++ tagcloud.php
@@ -0,0 +1,81 @@
+<?php
+/*
+Plugin Name: Tag Cloud widget for UTW
+Plugin URI: http://blog.broom9.com/?page_id=349
+Description: Adds a tag cloud sidebar widget, used with <a href="http://www.neato.co.nz/archives/2007/02/04/ultimate-tag-warrior-314159265/">Ultimate Tag Warrior 3.14159265</a>
+Author: Wei Wei
+Version: 1.0
+Author URI: http://www.broom9.com
+*/
+
+//Thanks for the google search widget( http://automattic.com ), I copied a lot of codes from it
+
+function widget_cloud_init() {
+
+	// Check for the required plugin functions. This will prevent fatal
+	// errors occurring when you deactivate the dynamic-sidebar plugin.
+	if ( !function_exists('register_sidebar_widget') )
+		return;
+
+	// This is the function that outputs our little form.
+	function widget_cloud($args) {
+		
+		// $args is an array of strings that help widgets to conform to
+		// the active theme: before_widget, before_title, after_widget,
+		// and after_title are the array keys. Default tags: li and h2.
+		extract($args);
+
+		// Each widget can store its own options. We keep strings here.
+		$options = get_option('widget_cloud');
+		$title = $options['title'];
+		$tagStyle = $options['tagStyle'];
+		
+		// These lines generate our output. Widgets can be very complex
+		// but as you can see here, they can also be very, very simple.
+		echo $before_widget . $before_title . $title . $after_title;
+		echo '<div>';
+		UTW_ShowWeightedTagSet($tagStyle);
+		echo '</div>';
+		echo $after_widget;
+	}
+
+	// This is the function that outputs the form to let the users edit
+	// the widget's title. It's an optional feature that users cry for.
+	function widget_cloud_control() {
+
+		// Get our options and see if we're handling a form submission.
+		$options = get_option('widget_cloud');
+		if ( !is_array($options) )
+			$options = array('title'=>'', 'tagStyle'=>__('sizedtagcloud', 'widgets'));
+		if ( $_POST['cloud-submit'] ) {
+
+			// Remember to sanitize and format use input appropriately.
+			$options['title'] = strip_tags(stripslashes($_POST['cloud-title']));
+			$options['tagStyle'] = strip_tags(stripslashes($_POST['cloud-tagStyle']));
+			update_option('widget_cloud', $options);
+		}
+
+		// Be sure you format your options to be valid HTML attributes.
+		$title = htmlspecialchars($options['title'], ENT_QUOTES);
+		$tagStyle = htmlspecialchars($options['tagStyle'], ENT_QUOTES);
+		// Here is our little form segment. Notice that we don't need a
+		// complete form. This will be embedded into the existing form.
+		echo '<p style="text-align:right;"><label for="cloud-title">' . __('Title:') . ' <input style="width: 200px;" id="cloud-title" name="cloud-title" type="text" value="'.$title.'" /></label></p>';
+		echo '<p style="text-align:right;"><label for="cloud-tagStyle">' . __('Tag Style:') . ' <input style="width: 200px;" id="cloud-tagStyle" name="cloud-tagStyle" type="text" value="'.$tagStyle.'" /></label></p>';
+		echo '<p><a target="_blank" href="http://www.neato.co.nz/wp-content/plugins/UltimateTagWarrior/ultimate-tag-warrior-help-themes.html#predefinedformats">Tag Style Reference</a></p>';
+		echo '<input type="hidden" id="cloud-submit" name="cloud-submit" value="1" />';
+	}
+	// This registers our widget so it appears with the other available
+	// widgets and can be dragged and dropped into any active sidebars.
+	register_sidebar_widget(array('Tag Cloud', 'widgets'), 'widget_cloud');
+	
+	// This registers our optional widget control form. Because of this
+	// our widget will have a button that reveals a 300x100 pixel form.
+	register_widget_control(array('Tag Cloud', 'widgets'), 'widget_cloud_control', 300, 150);
+	
+}
+
+// Run our code later in case this loads prior to any required plugins.
+add_action('widgets_init', 'widget_cloud_init');
+
+?>
